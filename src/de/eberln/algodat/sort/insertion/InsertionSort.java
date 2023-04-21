262740e482c64b6d2caee8a9c637169a368496b0
NicoE <nico@eberln.de>
2023-04-21 10:50:18 +0200

    normal insertion sort implementation

---
+++ src/de/eberln/algodat/sort/insertion/InsertionSort.java
@@ -0,0 +1,34 @@
+package de.eberln.algodat.sort.insertion;
+
+import de.eberln.algodat.sort.Sortable;
+
+public class InsertionSort<T extends Comparable<T>> extends Sortable<T>{
+
+	public InsertionSort(T... elements) {
+		super(elements);
+	}
+	
+	@Override
+	public T[] sort() {
+				
+		for(int i = 1; i<array.length; i++) {
+			
+			for(int j = i;j>0;j--) {
+				
+				if(array[j].compareTo(array[j-1]) < 0) {
+				
+					T tmp = array[j-1];
+					array[j-1] = array[j];
+					array[j] = tmp;
+					
+				}else {
+					break;
+				}
+			}
+		}
+		
+		return array;
+		
+	}
+	
+}
