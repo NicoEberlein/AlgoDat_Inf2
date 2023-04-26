a9126ff71cce9e158522aa920cde894cfcaf731b
NicoE <nico@eberln.de>
2023-04-26 15:24:15 +0200

    implment bubbleSort algorithm

---
+++ src/de/eberln/algodat/sort/bubble/BubbleSort.java
@@ -0,0 +1,40 @@
+package de.eberln.algodat.sort.bubble;
+
+import de.eberln.algodat.sort.Sortable;
+
+public class BubbleSort<T extends Comparable<T>> extends Sortable<T> {
+	
+	public BubbleSort(T... elements) {
+		super(elements);
+	}
+	
+	public T[] sort() {	
+		
+		for(int j = array.length-1; j>0; j--) {
+		
+			for(int i = 0; i<j; i++) {
+				
+				if(array[i].compareTo(array[i+1]) > 0) {
+					
+					swapAtIndex(i, i+1);
+					
+				}
+				
+			}
+			
+		}
+		
+		return array;
+		
+	}
+	
+	private void swapAtIndex(int i, int j) {
+		
+		T tmp = array[i];
+		array[i] = array[j];
+		array[j] = tmp;
+		
+	}
+	
+	
+}
